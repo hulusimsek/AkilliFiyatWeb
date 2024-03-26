b96f612eb1bf5e62713d07704ffcae8b683cae07
Hulusi Şimşek <hulusimsek@gmail.com>
2024-03-26 05:21:09 +0300

    first commit

---
+++ Entity/Urunler.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AkilliFiyatWeb.Entity
+{
+    public class Urunler
+    {
+        public string Id { get; set; }
+        public string UrunAdi { get; set; }
+        public string Fiyat { get; set; }
+        public string UrunResmi { get; set; }
+        public string MarketAdi { get; set; }
+        public string MarketResmi { get; set; }
+        public double? Benzerlik { get; set; }
+        public string AyrintiLink { get; set; }
+        public int Miktar { get; set; } = 0;
+        public string EskiFiyat { get; set; }
+        public double? IndirimOran { get; set; }
+        
+        public Urunler()
+        {
+            // Default constructor
+        }
+    }
+}
